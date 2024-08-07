6487f60c5ec5bdfe3fa90b065de857cbc438296b
QentiX <programmisteconomist21@gmail.com>
2024-08-07 23:06:02 +0300

    create: added model window & mobile responsive

--- js/main.js
+++ js/main.js
@@ -1,34 +1,64 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-	// Optional parameters
-	loop: true,
-	speed: 750,
-	autoplay: {
-		delay: 5000,
-	},
+$(document).ready(function () {
+	const hotelSlider = new Swiper('.hotel-slider', {
+		// Optional parameters
+		loop: true,
+		speed: 750,
+		autoplay: {
+			delay: 5000,
+		},
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.hotel-slider__button--next',
-		prevEl: '.hotel-slider__button--prev',
-	},
-})
+		// Navigation arrows
+		navigation: {
+			nextEl: '.hotel-slider__button--next',
+			prevEl: '.hotel-slider__button--prev',
+		},
+	})
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-	// Optional parameters
-	loop: true,
-	speed: 1000,
+	const reviewsSlider = new Swiper('.reviews-slider', {
+		// Optional parameters
+		loop: true,
+		speed: 1000,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.reviews-slider__button--next',
-		prevEl: '.reviews-slider__button--prev',
-	},
-})
+		// Navigation arrows
+		navigation: {
+			nextEl: '.reviews-slider__button--next',
+			prevEl: '.reviews-slider__button--prev',
+		},
+	})
 
-let menuButton = document.querySelector('.menu-button')
+	// slider
 
-menuButton.addEventListener('click', function () {
-	document
-		.querySelector('.navbar-bottom')
-		.classList.toggle('navbar-bottom--visible')
+	let menuButton = document.querySelector('.menu-button')
+
+	menuButton.addEventListener('click', function () {
+		document
+			.querySelector('.navbar-bottom')
+			.classList.toggle('navbar-bottom--visible')
+	})
+
+	// modal window
+
+	let modalButton = $('[data-toggle=modal]')
+	let closeModalButton = $('.modal__close')
+
+	modalButton.on('click', openModal)
+	closeModalButton.on('click', closeModal)
+
+	function openModal() {
+		let modalOverlay = $('.modal__overlay')
+		let modalDialog = $('.modal__dialog')
+
+		modalOverlay.addClass('modal__overlay--visible')
+		modalDialog.addClass('modal__dialog--visible')
+	}
+
+	function closeModal(event) {
+		event.preventDefault()
+
+		let modalOverlay = $('.modal__overlay')
+		let modalDialog = $('.modal__dialog')
+
+		modalOverlay.removeClass('modal__overlay--visible')
+		modalDialog.removeClass('modal__dialog--visible')
+	}
 })
